1fb355212252ad8d4d0260dadd0ff988510c0521
Marie Zamzhitskaya <marizamj@gmail.com>
2019-01-12 19:02:10 +0100

    Cleanup

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app__header">
+          <h1>GitHub Portfolio</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
