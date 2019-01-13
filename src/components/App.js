300321989015c8020562da1aeab347382de85934
Marie Zamzhitskaya <marizamj@gmail.com>
2019-01-14 00:11:36 +0100

    add react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <header className="app__header">
-          <h1>GitHub Portfolio</h1>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <header className="app__header">
+            <h1>GitHub Portfolio</h1>
+          </header>
+          <Route exact path="/" component={null} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
