854babcffc35af1219f06741c5dce5e05ca2c034
Marie Zamzhitskaya <marizamj@gmail.com>
2019-01-16 15:05:35 +0100

    eslint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import { MemoryRouter, Route } from 'react-router';
 import App from '../containers/App';
 import Search from '../components/Search';
